b01451fe079a93fdc634cc842ca0c382ce52eddc
mi2428 <mi2428782020@gmail.com>
2021-08-06 22:44:01 +0900

    Update templates
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- renderer.py
+++ renderer.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python3
-from jinja2 import Template
+from re import TEMPLATE
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+import yaml
 import os
+import glob
 
-network_config_tpl = os.path.join(os.path.dirname(__file__), "./system-boot/network-config.j2")
-user_data_tpl = os.path.join(os.path.dirname(__file__), "./system-boot/user-data.j2")
+BASE_PATH = os.path.dirname(__file__)
+TEMPLATES_BASE_PATH = os.path.join(BASE_PATH, "templates")
+CONFIGS_BASE_PATH = os.path.join(BASE_PATH, "system-boot")
+PARAMETERS_PATH = os.path.join(BASE_PATH, "params.yml")
+
+
+def boolstr(boolean):
+  return ["false", "true"][boolean]
 
 
 def main():
-  pass
+  env = Environment(loader=FileSystemLoader(TEMPLATES_BASE_PATH, encoding="utf_8"))
+  env.filters["boolstr"] = boolstr
+
+  params = None
+  with open(PARAMETERS_PATH) as fd:
+    params = yaml.safe_load(fd)
+  
+  for tpl_path in glob.glob(f"{TEMPLATES_BASE_PATH}/*.j2"):
+    tpl_name = os.path.basename(tpl_path)
+    tpl = env.get_template(tpl_name)
+    cfg_path = os.path.join(CONFIGS_BASE_PATH, tpl_name.split(".")[0])
+    with open(cfg_path, "w") as fd:
+      out = tpl.render(params)
+      fd.write(out)
 
 
 if __name__ == "__main__":
