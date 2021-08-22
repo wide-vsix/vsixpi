94baa8e550b5d85e939801c2ca9c487d98e0af4d
mi2428 <mi2428782020@gmail.com>
2021-08-22 19:06:29 +0900

    Reconfigure every configuration based on vsixpi.yml
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- renderer.py
+++ renderer.py
@@ -5,6 +5,7 @@ See: https://github.com/wide-vsix/cloud-init-vsixpi
 """
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
+from pprint import pprint
 import jinja2
 import yaml
 import os
@@ -32,11 +33,19 @@ def validate_yaml(data, on_error=None):
   else:
     return True, d
 
-def apply_result():
-  pass
+
+def provisioning(cloud_config):
+  if "write_files" not in cloud_config:
+    return
+
+  configs = cloud_config["write_files"]
+  for config in configs:
+    path, content = config["path"], config["content"]
+    with open(path, "w") as fd:
+      fd.write(content)
 
 
-def renderer(reconf=False):
+def render(reconf=False):
   env = Environment(loader=FileSystemLoader(TEMPLATES_BASE_PATH))
   env.filters["boolstr"] = boolstr
 
@@ -45,18 +54,22 @@ def renderer(reconf=False):
     ok, params = validate_yaml(fd, on_error="Failed to parse vsixpi.yml")
     if not ok:
       sys.exit(1)
-  
+
   for tpl_path in glob.glob(f"{TEMPLATES_BASE_PATH}/*.j2"):
     tpl_name = os.path.basename(tpl_path)
     tpl = env.get_template(tpl_name)
-    cfg_path = os.path.join(CONFIGS_BASE_PATH, ".".join(tpl_name.split(".")[:-1]))
-    
+    tpl_name_bare = ".".join(tpl_name.split(".")[:-1])
+    cfg_path = os.path.join(CONFIGS_BASE_PATH, tpl_name_bare)
+
     try:
       with open(cfg_path, "w") as fd:
         out = tpl.render(params)
-        ok, _ = validate_yaml(out, on_error=f"Failed to validate rendered config {cfg_path}")
+        ok, parsed = validate_yaml(out, on_error=f"Failed to validate rendered config {cfg_path}")
         if not ok:
           sys.exit(254)
+        if reconf:
+          #pprint(parsed)
+          provisioning(parsed)
         fd.write(out)
 
     except FileNotFoundError as e:
@@ -70,9 +83,9 @@ def renderer(reconf=False):
 
 def main():
   parser = argparse.ArgumentParser(description="A helper utility to setup vSIX Pi - see wide-vsix/vsixpi at GitHub for details.")
-  parser.add_argument("--reconfigure", action="store_true", help="Reconfigure running vSIX Pi (default: False)")
+  parser.add_argument("--reconfigure", action="store_true", help="Reconfigure running vSIX Pi, need to run with sudo (default: False)")
   args = parser.parse_args()
-  renderer(reconf=args.reconfigure)
+  render(reconf=args.reconfigure)
 
 
 if __name__ == "__main__":
